965a612a3033aec97206802f10c5c716d55ba9c1
宜志富紹太 <shotagishitomi@gmail.com>
2020-04-04 00:18:01 +0900

    新規追加！最後まで完成させる！

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT情報サイト</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div id="index">
+        <header>
+            <h1 class="serif">ITと就活の情報サイト</h1>
+
+        </header>
+        
+    </div>
+
+
+
+
+    <script src="./assets/js/jquery.js"></script>
+</body>
+
+</html>
