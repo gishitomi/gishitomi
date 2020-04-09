e5ad5fe0b27ef067d547bb64854bb2b56d15554b
宜志富紹太 <shotagishitomi@gmail.com>
2020-04-09 23:24:25 +0900

    ひとまず目標は達成したので完成

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
                         <li>IT関連のニュース <i class="fas fa-desktop"></i></li>
                     </a>
                     <a class="underline" id="scroll_btn3" href=" ">
-                        <li>IT業界のトレンド <i class=" fas fa-book-open "></i></li>
+                        <li>IT業界のトレンド <i class="fas fa-laptop-code"></i></li>
                     </a>
                     <a class=" underline" id="scroll_btn4" href=" ">
                         <li>勉強記録 <i class=" fas fa-book-open "></i></li>
@@ -62,6 +62,16 @@
             <div class=" gray-box ">
             </div>
             <div class=" topic ">
+                <img src="./assets/img/social-networks-1863613_1920.png" alt="">
+
+                <!-- <div class="topic-box" id="box1">
+                    <img src="./assets/img/twitter-312464_1280.png" alt="">
+                </div>
+                <div class="topic-box" id="box2">
+                    <img src="./assets/img/logo-2582757_1920.png" alt="">
+                </div>
+                <div class="topic-box" id="box3"></div>
+                <div class="topic-box" id="box4"></div> -->
 
             </div>
         </main>
@@ -71,29 +81,29 @@
                 <p>　　　　就職、経済に関するニュース、サイトのまとめ</p>
                 <div class=" note ">
                     <p>
-                    <?php
-                    $xmlTree = simplexml_load_file('https://news.yahoo.co.jp/pickup/economy/rss.xml');
-                    ?>
-                <a href=" <?php print($xmlTree->channel->link) ?>"><?php print($xmlTree->channel->title); ?></a>
-                    <?php
+                        <?php
+                        $xmlTree = simplexml_load_file('https://news.yahoo.co.jp/pickup/economy/rss.xml');
+                        ?>
+                        <a href=" <?php print($xmlTree->channel->link) ?>"><?php print($xmlTree->channel->title); ?></a>
+                        <?php
 
-                    echo "<br>";
-                    print($xmlTree->channel->description);
-                    echo "<br>";
-                    print($xmlTree->channel->pubDate);
-                    echo "<br>";
+                        echo "<br>";
+                        print($xmlTree->channel->description);
+                        echo "<br>";
+                        print($xmlTree->channel->pubDate);
+                        echo "<br>";
 
 
-                    foreach ($xmlTree->channel->item as $item) :
-                    ?>
-                        ・<a href=" <?php print($item->link); ?>"><?php print($item->title); ?></a>
+                        foreach ($xmlTree->channel->item as $item) :
+                        ?>
+                            ・<a href=" <?php print($item->link); ?>"><?php print($item->title); ?></a>
 
-                        <?php print("　　　　" . $item->pubDate); ?>
-                        <br>
-                        <p><?php print($item->description); ?></p>
-                    <?php endforeach; ?>
+                            <?php print("　　　　" . $item->pubDate); ?>
+                            <br>
+                            <p><?php print($item->description); ?></p>
+                        <?php endforeach; ?>
                     </p>
-            </div>
+                </div>
             </article>
             <article>
                 <h2 class=" it ">IT関連</h2>
@@ -134,14 +144,14 @@
                         $xmlTree = simplexml_load_file('https://qiita.com/tags/%E6%96%B0%E4%BA%BA%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E5%BF%9C%E6%8F%B4/feed.atom');
                         ?>
                         <a href="<?php print($xmlTree->link); ?>">
-                        <?php print($xmlTree->description); ?></a>
+                            <?php print($xmlTree->description); ?></a>
                         <br>
                         <?php
                         foreach ($xmlTree->entry as $entry) :
                         ?>
                             ・<a href="<?php print($entry->url); ?>">
-                        <?php print($entry->title); ?></a>
-                        <br>
+                                <?php print($entry->title); ?></a>
+                            <br>
                         <?php endforeach; ?>
 
                     </p>
@@ -151,12 +161,21 @@
                 <h2 class="dialy ">勉強記録</h2>
                 <p>　　　　日々の勉強を記録します</p>
                 <div class="note ">
-                    <p></p>
+                    <p>HTML</p>
+                    <br>
+                    <p>CSS</p>
+                    <br>
+                    <p>JavaScript</p>
+                    <br>
+                    <p>Python</p>
+                    <br>
+                    <p>PHP</p>
+                    <br>
                 </div>
             </article>
 
 
-    </div>
+        </div>
 
     </div>
 
